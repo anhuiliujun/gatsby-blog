4da4d205f72c58ed5d44f52a9351507e943ddf11
继道 <issac.lj@alibaba-inc.com>
2018-11-10 13:38:17 +0800

    Build Gatsby Page Slugs Dynamically from Markdown with gatsby-node.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import Header from '../components/Header';
 
 const Layout = ({ data }) => {
@@ -19,7 +19,9 @@ const Layout = ({ data }) => {
           const { frontmatter } = edge.node;
           return (
             <div key={frontmatter.path} style={{ marginBottom: '1rem' }}>
-              {frontmatter.title}
+              <Link to={frontmatter.path}>
+                {frontmatter.title}
+              </Link>
             </div>
           );
         })}
